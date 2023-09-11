a7a295d07717d135b94b3dd98cef3421d63052ef
Moreno Razzoli <morrolinux@gmail.com>
2023-09-11 15:17:33 +0200

    add coupon redeem instructions

---
+++ coupon_lpi.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<!--
+NAME: Morrolinux.it Website
+AUTHOR: Moreno Razzoli
+-->
+
+<html lang="it">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- Chrome Mobile Top Bar Color-->
+  <meta name="theme-color" content="#343a40" />
+
+  <meta name="description" content="Ottieni la certificazione Linux e inizia la tua carriera come professionista IT in un settore in rapida crescita grazie al corso bestseller italiano su Linux. Scopri la nostra selezione di corsi ideati su misura per te!">
+
+  <title>Morrolinux.it</title>
+
+</head>
+
+<body id="page-top">
+
+  <!-- #HOME -->
+  <section id="home">
+    <div class="container mt-5">
+	<h2 class="my-5 text-center">Richiedi il tuo COUPON LPI</h2>
+
+	<p> Come <b>Training Partner Autorizzato</b> della Linux Professional Institute (LPI), 
+		posso generare per i miei iscritti uno speciale <b>coupon sconto</b> per la prenotazione di <b>tutti gli esami LPI.</b> 
+		Compila il modulo di richiesta e ricevi il tuo coupon entro 48 ore con il seguente link: </p>
+	<br>
+        <div class="mb-5 text-center">
+        <a target="_blank" style="margin: 0 auto;" title="Arch Linux Per Comuni Mortali" href="https://forms.gle/9vxTEVDftBzioDat8" class="btn btn-primary mt-auto js-scroll-trigger">Richiedi il tuo COUPON</a>
+
+	<h2 class="my-5 text-center">Come si usa il COUPON LPI?</h2>
+
+	<p>Una volta ricevuto il codice coupon, potrai utilizzarlo per <b>acquistare il voucher d'esame ad un prezzo scontato</b> sul portale <a target=_blank href="https://people.lpi.org/vouchers">people.lpi.org</a>.<br>
+		La procedura di riscatto consiste in 4 semplici passaggi: <br><br> </p>
+
+	<p> 
+
+		<h4 class="text-center">1</h4>
+		Recati su <a target=_blank href="https://people.lpi.org/vouchers">people.lpi.org</a>
+		e dalla barra laterale seleziona "Shop" --> "Vouchers" e infine "BUY VOUCHERS" in alto a destra.<br>
+	    	<img src="img/LPI_COUPONS_1.png" width="80%"><br><br>
+		<h4 class="text-center">2</h4>
+		Seleziona la tipologia di esame che desideri sostenere:<br>
+		Serie "Essentials" (Linux Essentials, Security Essentials, ...) oppure "Professional" (LPIC-1, LPIC-2, ...) <br>
+	    	<img src="img/LPI_COUPONS_2.png" width="80%"><br><br>
+		<h4 class="text-center">3</h4>
+		Aggiungi l'esame al carrello.<br>
+	    	<img src="img/LPI_COUPONS_3.png" width="80%"><br><br>
+		<h4 class="text-center">4</h4>
+		Inserisci il codice coupon nella finestra di dialogo pop-up, premi "APPLY" e poi "CHECKOUT".<br>
+	    	<img src="img/LPI_COUPONS_4.png" width="80%"><br><br>
+	</p>
+
+	<h2 class="my-5 text-center">Come si prenota l'esame?</h2>
+	<p>
+		Una volta acquistato il voucher, potrai utilizzarlo per prenotare l'esame direttamente dal portale Pearson VUE.<br>
+		Per maggiori informazioni sulla procedura di prenotazione degli esami ti invito a consultare la <a target=_blank href="https://odoo.lpi.org/web/content/58272?access_token=4f7cc131-7670-4574-92b1-35bdde9a9a12">guida ufficiale</a> fornita da LPI.
+	</p>
+
+        </div>
+
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container footer">
+      <p class="m-0 text-center text-white">
+      <a style="opacity:0.5;color:white;" >Moreno Razzoli<br>P.IVA IT04063670360</a> <br>
+      <a style="opacity:0.5;color:white;" target="_blank" href="http://morrolinux.it/privacy_policy" title="Privacy Policy">Privacy Policy</a> 
+      <br>
+      </p>
+
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="js/jquery.fitvids.js"></script>
+
+  <!-- Custom JavaScript -->
+  <script src="js/script.js"></script>
+
+
+</body>
+
+</html>
